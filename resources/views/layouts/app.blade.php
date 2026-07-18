ecc921a28af60a215acd130131dd38c0e6746d31
Amit Biswas <amitbiswas8514@gmail.com>
2026-07-18 22:19:23 +0530

    Initial Laravel 10 installation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>BNYSVerse</title>
+
+</head>
+
+<body>
+
+<header>
+
+<h2>BNYSVerse</h2>
+
+<hr>
+
+</header>
+
+@yield('content')
+
+<hr>
+
+<footer>
+
+Copyright © BNYSVerse 2026
+
+</footer>
+
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Welcome to BNYSVerse 🚀</h1>
+
+<p>India's Professional Network for BNYS.</p>
+
+@endsection
